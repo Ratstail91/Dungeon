8ec76384cfb9b92ca9afe1b43e5f38bff5bdfaaa
Kayne Ruse <kayneruse@gmail.com>
2016-10-29 15:17:22 +1100

    Renamed a script

--- rsc/setup.lua
+++ rsc/setup.lua
@@ -13,19 +13,19 @@ tileSheetAPI = require("tile_sheet")
 
 print("map generators")
 mapMaker = require("map_maker")
-underdirk = require("underdirk")
+randomRooms = require("random_rooms")
 
 print("Attempting to set the map components")
 tileSheetAPI.Load("rsc/dungeon_sheet.png", 32, 32)
-regionPagerAPI.SetOnCreate(underdirk.Blank)
+regionPagerAPI.SetOnCreate(randomRooms.Blank)
 
 print("Attempting to generate a dungeon")
 
 local hearts = {}
 
-hearts[1] = underdirk.GenerateDungeon(1, 1, 50, 50, 10)
-hearts[2] = underdirk.GenerateDungeon(50, 1, 50, 50, 10)
+hearts[1] = randomRooms.GenerateDungeon(1, 1, 50, 50, 10)
+hearts[2] = randomRooms.GenerateDungeon(50, 1, 50, 50, 10)
 
-underdirk.GenPath(hearts[1][1], hearts[1][2], hearts[2][1], hearts[2][2])
+randomRooms.GenPath(hearts[1][1], hearts[1][2], hearts[2][1], hearts[2][2])
 
 print("setup.lua complete")
